283624aa87e884275205bc970e55e8c63de3b8fe
Thibaut COSENZA <thibautcosenza@gmail.com>
2023-09-06 11:25:40 +0200

    First

---
+++ exercice 1.2 2.py
@@ -0,0 +1,14 @@
+def sup(p:int, xargs):
+    for p in args:
+        print(p)
+sup(1, 5, 2, 3)
+a = input("Entrez votre nombre :")
+b = 10
+for i in list:
+    if [i] < b:
+        print('OK')
+        i = i + 1
+
+sup()
+
+#b = input("Changez le seuil:")
